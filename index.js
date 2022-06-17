26b0335ea0f73a3fed64535a91b08f01484eeb2e
Bipin Boga <bipinboga4601@gmail.com>
2022-06-17 13:06:22 +0530

    commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Msg = require('./models/messages')
 const app = express()
 const server = require('http').createServer(app)
 const port = process.env.PORT || 3000
 const io = require('socket.io')(server)
 const path = require('path')
+
+mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true })
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log("connection failed"))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
@@ -14,9 +20,13 @@ app.use(express.static(__dirname + '/public'))
 io.on('connection', socket => {
     // console.log('User Connected')
 
-    socket.on('chat', message => {
+    socket.on('chat', msg => {
         // console.log('From client: ', message)
-        io.emit('chat', message)
+        const message = new Msg({msg})
+        message.save().then(() => {
+            io.emit('chat', msg)
+        })
+        
       })
 })
 
